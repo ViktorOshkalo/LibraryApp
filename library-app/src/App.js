c73dd8b3bc2788d9cb62982abe96eaabc6293799
Viktor Oshkalo <vvicmail@gmail.com>
2018-05-30 00:12:26 +0300

    start development

--- library-app/src/App.js
+++ library-app/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './Closed_Book_Icon.svg';
 import './App.css';
 
+import {library} from './data/data.js'
+
 class App extends Component {
   render() {
     return (
@@ -11,7 +13,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to my LibraryApp</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. And fuck off.
+          To get started, edit <code>src/App.js</code> and save to reload. And fuck off with {library.books[0].title}
         </p>
       </div>
     );
